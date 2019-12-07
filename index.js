457259c43ef89355f226bc3f3ee38b2158cc3c3e
juan diego alejandro valencia pena <juandiegoalejandro@hotmail.com>
2019-12-07 21:54:28 +0000

    add feature to button update and delete user

--- index.js
+++ index.js
@@ -1,27 +1,61 @@
+function load_users() {
+  $.ajax({
+    type: 'GET',
+    url: 'http://127.0.0.1:8000/api/user/',
+    dataType:"json",
+    success: function (data) {
+        console.log(data)
+        let content = ''
+        for (i = 0; i < data.length; i++) {
+          content += '<tr>'
+          content += '<td>' + data[i].id + '</td>'
+          content += '<td> <input id="user_name_'+ data[i].id +'" type="text" value="' + data[i].name + '"></td>'
+          content += '<td>'
+          content += '<button id="update_user" onclick="update_user(' + data[i].id + ')" class="btn btn-primary">Update</button>'
+          content += '<button id="delete_user" onclick="delete_user(' + data[i].id + ')" class="btn btn-danger">Delete</button>'
+          content += '<button id="user_task" class="btn btn-info">Task</button>'
+        }
+        $('#user_info').html(content)
+    }
+  });
+}
+
+function update_user (user_id) {
+  $.ajax({
+    type: 'PUT',
+    url: 'http://127.0.0.1:8000/api/user/' + user_id + '/',
+    dataType:"json",
+    data:{name:$('#user_name_'+ user_id).val()},
+    success: function(data) {
+      console.log(data)
+      load_users()
+      alert("user updated")
+    }
+  })
+}
+
+function delete_user (user_id) {
+  $.ajax({
+    type: 'DELETE',
+    url: 'http://127.0.0.1:8000/api/user/' + user_id + '/',
+    dataType:"json",
+    success: function(data) {
+      console.log(data)
+      load_users() 
+    }
+  })
+}
+
+function user_task (user_id) {
+  
+}
+
 $(document).ready(function () {
+    load_users()
     $('button').on('click', function () {
       $('#userinfo').html("")
       $('#listofmatches').html("")
-        $.ajax({
-            type: 'GET',
-            url: 'http://127.0.0.1:8000/api/user/',
-            dataType:"json",
-            success: function (data) {
-                console.log(data)
-                let content = ''
-                for (i = 0; i < data.length; i++) {
-                  content += '<tr>'
-                  content += '<td>' + data[i].id + '</td>'
-                  content += '<td> <input type="text" value="' + data[i].name + '"></td>'
-                  content += '<td>'
-                  content += '<button class="btn btn-primary">Update</button>'
-                  content += '<button class="btn btn-danger">Delete</button>'
-                  content += '<button class="btn btn-info">Task</button>'
-                }
-                $('#userinfo').html(content)
-                
-            }
-          });
+        
         $.ajax({
             type: 'GET',
             url: '/bestmatch/' + $('input').val(),
